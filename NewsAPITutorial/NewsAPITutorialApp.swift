df0746d8187245fcaf476a2a379b9c4af2e575fe
sglee237 <sglee237@gmail.com>
2023-03-14 10:23:22 +0900

    Initial Commit

---
+++ NewsAPITutorial/NewsAPITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPITutorialApp.swift
+//  NewsAPITutorial
+//
+//  Created by sglee237 on 2023/03/14.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
